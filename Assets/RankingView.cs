25b58f512ea0191405216ddfcaa9ee9d4b2973ea
Sebastian Perez <sebastian.perez@192.168.1.7>
2025-06-16 13:44:04 -0300

    add cards to ranking

--- Assets/RankingView.cs
+++ Assets/RankingView.cs
@@ -3,10 +3,23 @@ using UnityEngine;
 public class RankingView : MonoBehaviour
 {
     public Animator rankingAnimator;
+    public GameObject cardPrefab;
+    public Transform contentParent;
     
     private static readonly int OnEnterScene = Animator.StringToHash("onEnterScene");
+    
     void Start()
     {
         rankingAnimator.SetTrigger(OnEnterScene);
+        LoadRanking();
+    }
+
+    private void LoadRanking()
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            var newCard = Instantiate(cardPrefab, contentParent);
+            // podés personalizar el contenido del card acá
+        }
     }
 }
